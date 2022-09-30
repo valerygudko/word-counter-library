2dc81242f01b589f63b84b1bccf5dfc5145eb332
Valery Gudko <valery.gudko@gmail.com>
2022-09-30 19:39:47 +0100

    Empty project

---
+++ src/test/java/com/exercise/wordcounterlibrary/WordCounterLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.wordcounterlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCounterLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
